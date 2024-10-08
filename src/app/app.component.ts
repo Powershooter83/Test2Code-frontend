0c84b5360d844868d086bd73c65effa03eb4afba
p20102 <yanicksilvan.romere@css.ch>
2024-10-07 20:06:02 -0700

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,21 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatOption, MatSelect} from '@angular/material/select';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {PageComponent} from './page/page.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {
+  MatDrawer,
+  MatDrawerContainer,
+  MatDrawerContent,
+  MatSidenav,
+  MatSidenavContainer
+} from '@angular/material/sidenav';
+import {HistoryService} from '../history.service';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +35,18 @@ import {PageComponent} from './page/page.component';
     MatOption,
     NgForOf,
     PageComponent,
+    MatToolbar,
+    MatIcon,
+    MatNavList,
+    MatListItem,
+    MatSidenav,
+    MatSidenavContainer,
+    MatDrawerContent,
+    MatDrawer,
+    MatDrawerContainer,
+    MatTooltip,
+    NgIf,
+    MatDivider,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -29,5 +54,18 @@ import {PageComponent} from './page/page.component';
 export class AppComponent {
   title = 'frontend';
 
+  constructor(private historyService: HistoryService) {
+  }
 
+  get history() {
+    return this.historyService.getHistory();
+  }
+
+  deleteItem(id: string) {
+
+  }
+
+  onButtonClick(id: string) {
+
+  }
 }
---
+++ src/history.service.ts
@@ -0,0 +1,124 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoryService {
+
+  constructor() {
+  }
+
+
+  getHistory() {
+    return [
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      }, {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      }, {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      }, {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+      {
+        id: '12312312312',
+        name: 'create_test',
+        creation_date: '01.01.2024'
+      },
+
+    ]
+  }
+}
