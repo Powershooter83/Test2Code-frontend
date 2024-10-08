3a7a7782c4cfccd29795f2076ee17f3e79af81b9
p20102 <yanicksilvan.romere@css.ch>
2024-10-08 09:35:53 -0700

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, withComponentInputBinding, withDebugTracing, withRouterConfig} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHighlightOptions} from 'ngx-highlightjs';
+import {provideHttpClient} from '@angular/common/http';
+import {provideClientHydration} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes,
+      withDebugTracing(),
+      withComponentInputBinding(),
+      withRouterConfig({paramsInheritanceStrategy: 'always'})), provideClientHydration(), provideAnimationsAsync(),
+    provideHttpClient(),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js'),
+      lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+    }),
+  ]
+
 };
---
+++ src/models/history.model.ts
@@ -0,0 +1,6 @@
+export interface HistoryEntry {
+  id: string;
+  method: string;
+  created_at: string;
+  testCases: string;
+}
