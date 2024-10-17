82353fbf352c13349cf884836aaefc16363c3ed4
p20102 <yanicksilvan.romere@css.ch>
2024-10-17 12:27:14 -0700

    fix design

--- src/app/history-empty/history-empty.component.ts
+++ src/app/history-empty/history-empty.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {MatCard, MatCardActions, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
 import {MatButton} from '@angular/material/button';
-import {HistoryService} from '../../service/history.service';
 import {I18nService} from '../i18n.service';
 import {i18n} from '../../models/i18n.model';
+import {ThemingService} from '../theming.service';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-history-empty',
@@ -15,19 +16,20 @@ import {i18n} from '../../models/i18n.model';
     MatButton,
     MatCardTitle,
     MatCardSubtitle,
+    NgClass,
+    NgIf,
   ],
   templateUrl: './history-empty.component.html',
   styleUrl: './history-empty.component.scss'
 })
 export class HistoryEmptyComponent {
 
-  constructor( protected i18nService: I18nService) {
-  }
+  protected readonly i18n = i18n;
 
+  constructor(protected i18nService: I18nService, protected themingService: ThemingService) {
+  }
 
   generateTest() {
     location.reload()
   }
-
-  protected readonly i18n = i18n;
 }
